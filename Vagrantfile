22e047574f7fb4c24d6a634c77100c7e17e7329e
Michael Chapman <woppin@gmail.com>
2014-09-23 01:19:11 +1000

    Add puppet provisioning for repos

--- Vagrantfile
+++ Vagrantfile
@@ -16,12 +16,12 @@ Vagrant.configure("2") do |config|
     end
 
     # Use puppet to prepare repos etc.
-    #config.vm.provision :puppet do |puppet|
-    #  puppet.module_path = "modules"
-    #  puppet.manifest_file = "site.pp"
-    #  puppet.hiera_config_path = "hiera/hiera.yaml"
-    #  puppet.working_directory = "/vagrant/hiera/data"
-    #end
+    config.vm.provision :puppet do |puppet|
+      puppet.module_path = "modules"
+      puppet.manifest_file = "site.pp"
+      puppet.hiera_config_path = "hiera/hiera.yaml"
+      puppet.working_directory = "/vagrant/hiera/data"
+    end
 
     # Install build tools
     centos.vm.provision :shell do |shell|
--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -3,6 +3,7 @@
 network=eth1
 domain='domain.name'
 desired_ruby=2.0.0
+desired_puppet=3.7.2
 proxy="${proxy:-}"
 
 while getopts "h?p:" opt; do
@@ -56,3 +57,51 @@ if [ "${ruby_version}" != "${desired_ruby}" ] ; then
   yum install -y ruby ruby-devel
 fi
 
+# Ruby-augeas is needed for puppet, but is handled separately
+# since it requires native extensions to be compiled
+if ! gem list | grep ruby-augeas ; then
+    # comes from updates repo
+    yum install gcc augeas-devel -y -q
+    gem install ruby-augeas --no-ri --no-rdoc
+    yum remove -y -q gcc cpp
+fi
+
+# Install puppet from gem. This is not best practice, but avoids
+# repackaging large numbers of rpms and debs for ruby 2.0.0
+hash puppet 2>/dev/null || {
+  puppet_version=0
+}
+
+if [ "${puppet_version}" != '0' ] ; then
+    puppet_version=$(puppet --version)
+fi
+
+if [ "${puppet_version}" != "${desired_puppet}" ] ; then
+    gem install puppet --no-ri --no-rdoc
+fi
+
+# Ensure puppet user and group are configured
+if ! grep puppet /etc/group; then
+    echo 'adding puppet group'
+    groupadd puppet
+fi
+if ! grep puppet /etc/passwd; then
+    echo 'adding puppet user'
+    useradd puppet -g puppet -d /var/lib/puppet -s /sbin/nologin
+fi
+
+# Set up minimal puppet directory structure
+if [ ! -d /etc/puppet ]; then
+    echo 'creating /etc/puppet'
+    mkdir /etc/puppet
+fi
+
+if [ ! -d /etc/puppet/manifests ]; then
+    echo 'creating /etc/puppet/manifests'
+    mkdir /etc/puppet/manifests
+fi
+
+if [ ! -d /etc/puppet/modules ]; then
+    echo 'creating /etc/puppet/modules'
+    mkdir /etc/puppet/modules
+fi
